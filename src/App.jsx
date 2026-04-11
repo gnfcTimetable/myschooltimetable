9486f057965ba2d9e9f57a1dcb4e81b1b89cf6b3
gnfcTimetable <examinationgnfcs@gmail.com>
2026-04-11 10:50:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import TimetableTable from "./components/TimetableTable.jsx";
-
-import "./App.css"; // Ensure styles are applied
+import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="title">Guru Nanak Fifth Centenary School, Mussoorie</h1>
-      <TimetableTable />
-    </div>
+    <TimetableTable />
   );
 }
 
